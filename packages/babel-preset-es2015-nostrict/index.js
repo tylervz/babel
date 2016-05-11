2c56d4214c0fd635f970410e0018266783dca82a
alexykot <alexykot@gmail.com>
2016-05-11 06:53:42 +0100

    - reenabled back the plugin and added strict:false option

--- packages/babel-preset-es2015-nostrict/index.js
+++ packages/babel-preset-es2015-nostrict/index.js
@@ -19,7 +19,7 @@ module.exports = {
     require("babel-plugin-transform-es2015-destructuring"),
     require("babel-plugin-transform-es2015-block-scoping"),
     require("babel-plugin-transform-es2015-typeof-symbol"),
-    //require("babel-plugin-transform-es2015-modules-commonjs"),
+    [require("babel-plugin-transform-es2015-modules-commonjs"), {strict: false}],
     [require("babel-plugin-transform-regenerator"), { async: false, asyncGenerators: false }],
   ]
 };
